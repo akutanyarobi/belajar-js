56486baec70168993d8dc54b2ce93141d64b88ce
robikasep <46043848+robikasep@users.noreply.github.com>
2020-04-08 11:03:11 +0700

    pelajaran-02

---
+++ 02-deklarasi.js
@@ -0,0 +1,15 @@
+var emberVar = 'air';
+
+var emberVar = 'pasir';
+
+console.log(emberVar);
+
+let emberLet = 'kerikil';
+emberLet = 'tanah';
+
+console.log(emberLet);
+
+const emberConst = 'baju';
+emberConst = 'buah';
+
+console.log(emberConst);
