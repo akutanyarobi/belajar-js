9c3e38377984ac85dbfa0cfbc81f16d71b2ad610
akutanyarobi <63582690+akutanyarobi@users.noreply.github.com>
2020-04-22 10:54:29 +0700

    pelajaran-08

---
+++ 07-operator-perbandingan.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 10;
+
+let w = '5';
+
+// console.log(a > b);
+// console.log(a < b);
+
+// console.log(a == b);
+// console.log(a == a);
+// console.log(a == w);
+
+// console.log(a === b);
+// console.log(a === a);
+// console.log(a === w);
+
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a !== w);
+
+console.log(a >= b);
+console.log(a <= b);
