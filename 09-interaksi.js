d00baa1de23ca2f4304e1373c9329ed9cc5d900a
akutanyarobi <63582690+akutanyarobi@users.noreply.github.com>
2020-04-22 11:03:58 +0700

    pelajaran-10

---
+++ 09-interaksi.js
@@ -0,0 +1,12 @@
+// alert
+// alert('halo kawan');
+
+// prompt
+// let umur = prompt('berapa umur kamu');
+
+// alert(`Umur kamu ${umur} tahun`);
+
+// confirm
+let kamu = confirm('Kamu yakinnn ?');
+
+alert(kamu);
