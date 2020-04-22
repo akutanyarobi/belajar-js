e4acfebee54fede377c8583e491edceacc33cafd
akutanyarobi <63582690+akutanyarobi@users.noreply.github.com>
2020-04-22 10:59:17 +0700

    pelajaran-09

---
+++ 08-operator-logika.js
@@ -0,0 +1,34 @@
+/*
+// operator logika
+
+// kebalikan
+console.log(!true);
+console.log(!false);
+
+// or atau
+console.log('kucing' || 'burung');
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
+// and dan
+console.log('kucing' && 'burung');
+console.log(true && false);
+console.log(false && false);
+console.log(false && true);
+*/
+
+const x = 7;
+const y = 4;
+
+// kebalikan
+console.log(!x > y);
+console.log(x > y);
+
+// or / atau
+console.log(x == 5 || y == 5);
+console.log(x == 7 || y == 0);
+
+// and / dan
+console.log(x < 10 && y > 1);
+console.log(x < 10 && y < 1);
