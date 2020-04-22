8fe4a7bccbb94b02e77ff6d197c29911ac678d23
akutanyarobi <63582690+akutanyarobi@users.noreply.github.com>
2020-04-22 10:48:00 +0700

    pelajaran-06

---
+++ 05-operator-arimatika.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 5;
+
+let pertambahan = a + b;
+let pengurangan = a - b;
+let perkalian = a * b;
+let pemangkatan = a ** b;
+let pembagian = a / b;
+let modulus = a % b;
+
+console.log(pertambahan);
+console.log(pengurangan);
+console.log(perkalian);
+console.log(pemangkatan);
+console.log(pembagian);
+console.log(modulus);
+console.log(64 % 2);
+console.log(53 % 2);
